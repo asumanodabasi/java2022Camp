c2fee8b91481ad4e761331e70493db5bba2bfa54
ASUMAN ODABAŞI <103738413+asumanodabasi@users.noreply.github.com>
2022-09-28 12:13:02 +0300

    Java lesson 2

---
+++ javaweek2/oop1/src/oop1/Product.java
@@ -0,0 +1,52 @@
+package oop1;
+
+//PascalCase
+public class Product {
+	private String name;
+//camelCase for field
+	private double unitPrice;
+	private double disCount;
+	private String imageUrl;
+	private int unitsInStock;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public double getDisCount() {
+		return disCount;
+	}
+
+	public void setDisCount(double disCount) {
+		this.disCount = disCount;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public int getUnitsInStock() {
+		return unitsInStock;
+	}
+
+	public void setUnitsInStock(int unitsInStock) {
+		this.unitsInStock = unitsInStock;
+	}
+
+}
