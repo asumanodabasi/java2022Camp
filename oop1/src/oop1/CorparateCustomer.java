391d5c47f54e77513115b8ec45aa1cf0dc0bab8e
ASUMAN ODABAŞI <103738413+asumanodabasi@users.noreply.github.com>
2022-09-28 12:12:06 +0300

    Java lesson 2

---
+++ oop1/src/oop1/CorparateCustomer.java
@@ -0,0 +1,23 @@
+package oop1;
+
+public class CorparateCustomer extends Customer {
+	private String companyName;
+	private String taxNumber;
+
+	public String getTaxNumber() {
+		return taxNumber;
+	}
+
+	public void setTaxNumber(String taxNumber) {
+		this.taxNumber = taxNumber;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+}
