fe9100c3d9458046e24a5d30c103f95901249988
ASUMAN ODABAŞI <103738413+asumanodabasi@users.noreply.github.com>
2022-10-17 09:03:56 +0300

    Delete oopWithNLayerdApp directory

--- oopWithNLayerdApp/src/oopWithNLayerdApp/core/logging/Logger.java
+++
@@ -1,6 +0,0 @@
-package oopWithNLayerdApp.core.logging;
-
-public interface Logger {
-	void log(String data);
-
-}
--- oopWithNLayerdApp/src/oopWithNLayerdApp/dataAccess/ProductDao.java
+++
@@ -1,8 +0,0 @@
-package oopWithNLayerdApp.dataAccess;
-
-import oopWithNLayerdApp.entities.Product;
-
-public interface ProductDao { //interfaceler sadece metot imzasını barındırabilir!
-	void add(Product product);
-
-}
