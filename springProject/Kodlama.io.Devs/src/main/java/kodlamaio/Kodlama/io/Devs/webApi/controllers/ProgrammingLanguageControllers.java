4c10e7a6ee666943c6da062272a5f63755814b5b
ASUMAN ODABAŞI <103738413+asumanodabasi@users.noreply.github.com>
2022-11-02 00:07:42 +0300

    java spring project created

---
+++ springProject/Kodlama.io.Devs/src/main/java/kodlamaio/Kodlama/io/Devs/webApi/controllers/ProgrammingLanguageControllers.java
@@ -0,0 +1,56 @@
+package kodlamaio.Kodlama.io.Devs.webApi.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.Kodlama.io.Devs.business.abstracts.LanguageService;
+import kodlamaio.Kodlama.io.Devs.entities.concrete.ProgrammingLanguage;
+
+@RestController
+@RequestMapping("/api/programminglanguages")
+public class ProgrammingLanguageControllers {
+	private LanguageService languageService;
+
+	@Autowired
+	public ProgrammingLanguageControllers(LanguageService languageService) {
+		super();
+		this.languageService = languageService;
+	}
+	
+	@GetMapping ("/getall")
+	public List<ProgrammingLanguage> getAll()
+	{
+		return languageService.getAll();
+	}
+	
+	@PostMapping("/add")
+	public void add(@RequestBody ProgrammingLanguage language)
+	{
+	languageService.add(language);	
+	}
+	
+	@PostMapping("/delete")
+	public void delete(@RequestBody ProgrammingLanguage language)
+	{
+		languageService.delete(1);
+	}
+	
+	@PostMapping("/update")
+	public void update(@RequestBody ProgrammingLanguage language)
+	{
+		languageService.update(language);
+	}
+		
+	@PostMapping("/getbyid")
+	public void getById(@RequestBody ProgrammingLanguage language)
+	{
+		languageService.getById(language);
+	}
+	
+}
